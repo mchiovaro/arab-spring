e4a1b7ce0c4ecd59ab8c71f7473616ae0e9d0bb5
Megan Chiovaro <megan.chiovaro@uconn.edu>
2020-06-02 19:45:08 -0400

    Join CRPs for secondary Phoenix analyses

--- scripts/secondary-analyses/phoenix/02_crqa.R
+++ scripts/secondary-analyses/phoenix/02_crqa.R
@@ -117,6 +117,18 @@ par = list(unit = 2,
 plotRP(cross_recurrence_analysis$RP, par)
 dev.off()
 
+## join source-target plots ##
+
+# read in CRPs
+crp_1 <- readPNG('./results/secondary/phoenix/crqa/source_target-rp-all_events.png')
+crp_2 <- readPNG('./results/secondary/phoenix/crqa/source_target-rp-pos_events.png')
+crp_3 <- readPNG('./results/secondary/phoenix/crqa/source_target-rp-neg_events.png')
+
+# join images and save
+png("./results/secondary/phoenix/crqa/source_target-rp-joined.png")
+grid.arrange(rasterGrob(crp_1),rasterGrob(crp_2), rasterGrob(crp_3), ncol=3)
+dev.off()
+
 ### target filtered data ###
 
 ## count of all events and social cohesion ##
@@ -204,6 +216,18 @@ par = list(unit = 2,
            cols = "blue", 
            pcex = .5)
 plotRP(cross_recurrence_analysis$RP, par)
+dev.off()
+
+## join target plots ##
+
+# read in CRPs
+crp_1 <- readPNG('./results/primary/crqa/target-rp-all_events.png')
+crp_2 <- readPNG('./results/primary/crqa/target-rp-pos_events.png')
+crp_3 <- readPNG('./results/primary/crqa/target-rp-neg_events.png')
+
+# join images and save
+png("./results/primary/crqa/target-rp-joined.png")
+grid.arrange(rasterGrob(crp_1),rasterGrob(crp_2), rasterGrob(crp_3), ncol=3)
 dev.off()
 
 #### 3. Conduct permutation tests ####
