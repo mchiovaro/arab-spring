11d9202c130f2590d1a2453881204e35b8629344
Megan Chiovaro <megan.chiovaro@uconn.edu>
2020-07-29 11:01:35 -0400

    Update cross-wavelet script

--- scripts/primary-analyses/04_cross_wavelet.R
+++ scripts/primary-analyses/04_cross_wavelet.R
@@ -8,6 +8,8 @@
 # University of Connecticut
 # Last updated: 2020_05_15
 
+#### 1. Set up ####
+
 # clear environment
 rm(list=ls())
 
@@ -19,26 +21,37 @@ library(dplyr)
 library(biwavelet)
 
 # read in truncated data 
-data <- read.csv("./data/formatted/formatted_data.csv")
+data <- read.csv("./data/formatted/primary/formatted_data.csv")
 
 # make date into a number for the analysis
 data$date_num <- seq(from =1, to = nrow(data))
 
-t1 <- data[,c("date_num", "coh_dec")]
-t2 <- data[,c("date_num", "all_dec")]
+# read in time series
+coh_dec <- data[,c("date_num", "coh_deciles")]
+all_dec_targ <- data[,c("date_num", "all_deciles_target")]
+pos_dec_targ <- data[,c("date_num", "pos_deciles_target")]
+neg_dec_targ <- data[,c("date_num", "neg_deciles_target")]
+all_dec_source_targ <- data[,c("date_num", "all_deciles_source_target")]
+pos_dec_source_targ <- data[,c("date_num", "pos_deciles_source_target")]
+neg_dec_source_targ <- data[,c("date_num", "neg_deciles_source_target")]
 
-##### Coherence #####
+#### Run Cross-Wavelet Analysis ####
 
-# Specify the number of iterations. The more, the better (>1000)
+# specify the number of iterations (>1000 is best)
 nrands = 1000
 
+### target filtered data ###
+
+## count of all events and social cohesion ##
+
 # compute coherence
-wtc.AB = wtc(t1, t2, nrands = nrands)
+wtc.AB = wtc(coh_dec, all_dec_targ, nrands = nrands)
 
 # make room for the color bar on the right
 par(oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
 
 # plot the graph
+png('./results/primary/cross-wavelet/target-xw-all_events.png')
 plot(wtc.AB, 
      plot.phase = TRUE, 
      lty.coi = 1, 
@@ -50,22 +63,127 @@ plot(wtc.AB,
      ylab = "Scale", 
      xlab = "Period (days)", 
      plot.cb = TRUE, 
-     main = "Wavelet Coherence\nMean social coherence and Count of events (deciles)")
+     main = "Wavelet Coherence\nTwitter social cohesion and count of all events (deciles)\nTarget filtered data")
+dev.off()
 
-# Adding grid lines
-n = length(t1[, 1])
-abline(v = seq(5, n, 5), h = 1:16, col = "brown", lty = 1, lwd = 1)
+## count of positive events and social cohesion ##
 
-# Defining x labels
-#axis(side = 3, at = c(seq(5, n, 5)), labels = c(seq(5, 75, 5)))
-
-##### Cross Wavelet #####
-
-# Compute cross-wavelet
-xwt <- xwt(t1, t2)
+# compute coherence
+wtc.AB = wtc(coh_dec, pos_dec_targ, nrands = nrands)
 
 # make room for the color bar on the right
-par(oma=c(0, 0, 0, 1), mar=c(5, 4, 4, 5) + 0.1) 
+par(oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
 
-# Plot cross wavelet power and phase difference (arrows)
-plot(xwt, plot.cb=TRUE)
+# plot the graph
+png('./results/primary/cross-wavelet/target-xw-pos_events.png')
+plot(wtc.AB, 
+     plot.phase = TRUE, 
+     lty.coi = 1, 
+     col.coi = "grey", 
+     lwd.coi = 2, 
+     lwd.sig = 2, 
+     arrow.lwd = 0.03, 
+     arrow.len = 0.12, 
+     ylab = "Scale", 
+     xlab = "Period (days)", 
+     plot.cb = TRUE, 
+     main = "Wavelet Coherence\nTwitter social cohesion and count of positive events (deciles)\nTarget filtered data")
+dev.off()
+
+## count of negative events and social cohesion ##
+
+# compute coherence
+wtc.AB = wtc(coh_dec, neg_dec_targ, nrands = nrands)
+
+# make room for the color bar on the right
+par(oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
+
+# plot the graph
+png('./results/primary/cross-wavelet/target-xw-neg_events.png')
+plot(wtc.AB, 
+     plot.phase = TRUE, 
+     lty.coi = 1, 
+     col.coi = "grey", 
+     lwd.coi = 2, 
+     lwd.sig = 2, 
+     arrow.lwd = 0.03, 
+     arrow.len = 0.12, 
+     ylab = "Scale", 
+     xlab = "Period (days)", 
+     plot.cb = TRUE, 
+     main = "Wavelet Coherence\nTwitter social cohesion and count of negative events (deciles)\nTarget filtered data")
+dev.off()
+
+### source and target filtered data ###
+
+## count of all events and social cohesion ##
+
+# compute coherence
+wtc.AB = wtc(coh_dec, all_dec_source_targ, nrands = nrands)
+
+# make room for the color bar on the right
+par(oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
+
+# plot the graph
+png('./results/primary/cross-wavelet/source_target-xw-all_events.png')
+plot(wtc.AB, 
+     plot.phase = TRUE, 
+     lty.coi = 1, 
+     col.coi = "grey", 
+     lwd.coi = 2, 
+     lwd.sig = 2, 
+     arrow.lwd = 0.03, 
+     arrow.len = 0.12, 
+     ylab = "Scale", 
+     xlab = "Period (days)", 
+     plot.cb = TRUE, 
+     main = "Wavelet Coherence\nTwitter social cohesion and count of all events (deciles)\nSource and Target filtered data")
+dev.off()
+
+## count of positive events and social cohesion ##
+
+# compute coherence
+wtc.AB = wtc(coh_dec, pos_dec_source_targ, nrands = nrands)
+
+# make room for the color bar on the right
+par(oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
+
+# plot the graph
+png('./results/primary/cross-wavelet/source_target-xw-pos_events.png')
+plot(wtc.AB, 
+     plot.phase = TRUE, 
+     lty.coi = 1, 
+     col.coi = "grey", 
+     lwd.coi = 2, 
+     lwd.sig = 2, 
+     arrow.lwd = 0.03, 
+     arrow.len = 0.12, 
+     ylab = "Scale", 
+     xlab = "Period (days)", 
+     plot.cb = TRUE, 
+     main = "Wavelet Coherence\nTwitter social cohesion and count of positive events (deciles)\nSource and Target filtered data")
+dev.off()
+
+## count of negative events and social cohesion ##
+
+# compute coherence
+wtc.AB = wtc(coh_dec, neg_dec_source_targ, nrands = nrands)
+
+# make room for the color bar on the right
+par(oma = c(0, 0, 0, 1), mar = c(5, 4, 5, 5) + 0.1)
+
+# plot the graph
+png('./results/primary/cross-wavelet/source_target-xw-neg_events.png')
+plot(wtc.AB, 
+     plot.phase = TRUE, 
+     lty.coi = 1, 
+     col.coi = "grey", 
+     lwd.coi = 2, 
+     lwd.sig = 2, 
+     arrow.lwd = 0.03, 
+     arrow.len = 0.12, 
+     ylab = "Scale", 
+     xlab = "Period (days)", 
+     plot.cb = TRUE, 
+     main = "Wavelet Coherence\nTwitter social cohesion and count of negative events (deciles)\nSource and Target filtered data")
+dev.off()
